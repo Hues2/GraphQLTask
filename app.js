d5433e8dceb3d0c1995289a1b56a55da9dace2c0
GREG ROSS <1809717@chester.ac.uk>
2022-02-01 22:23:58 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const { default: axios } = require('axios');
+const express = require('express');
+const cors = require('cors')
+const app = express();
+app.use(cors());
+
+
+let graphQLData = {};
+
+function getData(){
+    axios.get('https://www.greenbowsports.co.uk/graphql?query={products(search: "forza-mini-target-goal-only"){items {name short_description {html} id sku image {url} price {regularPrice{ amount {value currency}}}}}}')
+    .then(response => {
+        console.log(response.data.data.products);
+        graphQLData = response.data.data.products
+    })
+
+}
+
+getData()
+
+app.get('/', (req, res) => {
+    res.json(graphQLData)
+});
+
+app.listen(3000);
---
+++ test.js
@@ -0,0 +1,1 @@
+fetch("http://localhost:3000").then(res => res.json()).then(data => console.log(data));
