3a49c3100862f064d8b0b3a354a395347a1a1fed
GREG ROSS <1809717@chester.ac.uk>
2022-02-02 22:27:24 +0000

    Added css, search bar and buttons for next and previous page

--- client/main.js
+++ client/main.js
@@ -1,35 +1,102 @@
 var listOfProductObjects = [];
-var pageNumber = 0;
+var pageNumber = 1;
 const row = document.getElementById("row");
 
-async function getData(){
-    listOfProductObjects = await fetch("http://localhost:3000").then(res => res.json()).then(data => {
-        return data.items;
+async function getData() {
+  listOfProductObjects = await fetch("http://localhost:3000")
+    .then((res) => res.json())
+    .then((data) => {
+      return data.items;
     });
 }
 
-function appendData(){
-    console.log("Before");
-    getData()
-    .then(() =>{
-        displayFirstPage();
-    });
+function appendData() {
+  getData().then(() => {
+    displayFirstPage();
+  });
 }
 
-function displayFirstPage(){
-    
-    for (let i = 0; i < 4; i++) {
-        var column = document.createElement("div");
-        column.setAttribute('id', 'column');
-        var img = document.createElement("img");
-        var h3 = document.createElement("h3");
-        img.src = listOfProductObjects[i].image.url;
-        h3.textContent = 'Name: ' + listOfProductObjects[i].name;
-        column.appendChild(img);
-        column.appendChild(h3);
-        row.appendChild(column);
-    }
+function displayFirstPage() {
+    while (row.firstChild){
+        row.firstChild.remove();
+    } 
+  for (let i = 0; i < 4; i++) {
+    var column = document.createElement("div");
+    column.setAttribute("id", "column");
+    var img = document.createElement("img");
+    var h2 = document.createElement("h2");
+    var h3 = document.createElement("h3");
+    img.src = listOfProductObjects[i].image.url;
+    h2.textContent = listOfProductObjects[i].name;
+    h3.textContent = listOfProductObjects[i].price.regularPrice.amount.value + " " + listOfProductObjects[i].price.regularPrice.amount.currency
+    column.appendChild(img);
+    column.appendChild(h2);
+    column.appendChild(h3);
+    row.appendChild(column);
+  }
+  pageNumber = 1;
 }
 
+function displaySecondPage() {
+    while (row.firstChild){
+        row.firstChild.remove();
+    } 
+  pageNumber = 2;
+  for (let i = 4; i < 8; i++) {
+    var column = document.createElement("div");
+    column.setAttribute("id", "column");
+    var img = document.createElement("img");
+    var h2 = document.createElement("h2");
+    var h3 = document.createElement("h3");
+    img.src = listOfProductObjects[i].image.url;
+    h2.textContent = listOfProductObjects[i].name;
+    h3.textContent = listOfProductObjects[i].price.regularPrice.amount.value + " " + listOfProductObjects[i].price.regularPrice.amount.currency
+    column.appendChild(img);
+    column.appendChild(h2);
+    column.appendChild(h3);
+    row.appendChild(column);
+  }
+  pageNumber = 2;
+}
 
+function displayThirdPage() {
+  while (row.firstChild){
+      row.firstChild.remove();
+  }  
+  pageNumber = 3;
+  for (let i = 8; i < 12; i++) {
+    var column = document.createElement("div");
+    column.setAttribute("id", "column");
+    var img = document.createElement("img");
+    var h2 = document.createElement("h2");
+    var h3 = document.createElement("h3");
+    img.src = listOfProductObjects[i].image.url;
+    h2.textContent = listOfProductObjects[i].name;
+    h3.textContent = listOfProductObjects[i].price.regularPrice.amount.value + " " + listOfProductObjects[i].price.regularPrice.amount.currency
+    column.appendChild(img);
+    column.appendChild(h2);
+    column.appendChild(h3);
+    row.appendChild(column);
+  }
+  pageNumber = 3;
+}
 
+function nextButton() {
+  if (pageNumber == 1) {
+    displaySecondPage();
+  } else if (pageNumber == 2) {
+    displayThirdPage();
+  } else {
+    displayFirstPage();
+  }
+}
+
+function previousButton() {
+  if (pageNumber == 1) {
+    displayThirdPage();
+  } else if (pageNumber == 2) {
+    displayFirstPage();
+  } else {
+    displaySecondPage();
+  }
+}
--- server/app.js
+++ server/app.js
@@ -1,26 +1,39 @@
 const { default: axios } = require('axios');
 const express = require('express');
 const cors = require('cors')
+const bodyParser = require('body-parser');
 const app = express();
 app.use(cors()); 
+app.use(bodyParser.urlencoded({extended: true}))
+
+var userInput = "\"forza-mini-target-goal-only\"";
 
 
 let graphQLData = {};
 
 function getData(){
-    axios.get('https://www.greenbowsports.co.uk/graphql?query={products(search: "forza-mini-target-goal-only"){items {name short_description {html} id sku image {url} price {regularPrice{ amount {value currency}}}}}}')
+    axios.get("https://www.greenbowsports.co.uk/graphql?query={products(search: " + userInput + "){items {name short_description {html} id sku image {url} price {regularPrice{ amount {value currency}}}}}}")
     .then(response => {
         console.log(response.data.data.products);
         graphQLData = response.data.data.products
+    }).catch(error =>{
+        graphQLData = {};
     })
 
 }
 
-getData()
 
 app.get('/', (req, res) => {
+    
     res.json(graphQLData)
     console.log("GET request received");
 });
 
+app.get('/search', (req, res) => {
+    console.log("POST received");
+    userInput = "\"" + req.body.product + "\"";
+    
+})
+
+getData()
 app.listen(3000);
